8350225611c9505041dd0c15109eebc7dfcd1157
Michel de Lima Soares <msoares@agecomnet.com.br>
2025-04-10 21:45:27 -0300

    fix bat quebra linha

--- build_app_e_compose.bat
+++ build_app_e_compose.bat
@@ -6,7 +6,7 @@ git config --global core.autocrlf false
 echo.
 echo =======================================
 echo Convertendo arquivos .sh para LF...
-powershell -Command "Get-ChildItem -Path ./scripts -Filter *.sh -Recurse | ForEach-Object { (Get-Content $_.FullName) | Set-Content -NoNewline -Encoding ascii $_.FullName }"
+powershell -Command "Get-ChildItem -Path ./scripts -Filter *.sh -Recurse | ForEach-Object { (Get-Content $_.FullName) -join \"`n\" | Set-Content -Encoding ascii $_.FullName }"
 
 echo.
 echo =======================================
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
 echo "Aguardando SQL Server iniciar..."
 sleep 20
-
 /opt/mssql-tools/bin/sqlcmd -S sqlserver -U SA -P "dev#appgestao202134" -i /scripts/sql_gestao_clientes.sql
